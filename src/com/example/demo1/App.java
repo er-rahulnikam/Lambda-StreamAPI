4fa8e640abf7b7a57239856aae6a50db18f44686
er-rahulnikam <errahulnikam@gmail.com>
2025-01-08 20:47:14 +0530

    Lambda expression is created

--- src/com/example/demo1/App.java
+++ src/com/example/demo1/App.java
@@ -4,5 +4,18 @@ public class App {
 	public static void main(String[] args) {
 		Greeting greet = new Hello();
 		greet.sayHello();
+		
+		Greeting greet2 = new Greeting() {
+			
+			@Override
+			public void sayHello() {
+				System.out.println("Anonymus sayHello method called");
+			}
+		};
+		greet2.sayHello();
+		
+		Greeting greet3 = () -> System.out.println("Lambda Expression");
+		greet3.sayHello();
+		
 	}
 }
