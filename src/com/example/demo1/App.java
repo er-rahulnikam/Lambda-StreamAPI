660f4abf607c775f15ed4a12b4294529d4c72a08
er-rahulnikam <errahulnikam@gmail.com>
2025-01-08 20:58:47 +0530

    Calculator interface and lambda expression is added for it

--- src/com/example/demo1/App.java
+++ src/com/example/demo1/App.java
@@ -1,5 +1,7 @@
 package com.example.demo1;
 
+import java.util.Random;
+
 public class App {
 	public static void main(String[] args) {
 		Greeting greet = new Hello();
@@ -17,5 +19,11 @@ public class App {
 		Greeting greet3 = () -> System.out.println("Lambda Expression");
 		greet3.sayHello();
 		
+		Calculator calculator = (int x, int y) -> {
+			Random random = new Random();
+			int randomNumber = random.nextInt();
+			return x*y + randomNumber;
+		};
+		System.out.println(calculator.calculate(1, 2));
 	}
 }
